bdf3611eb2afb59cd089608e4c41876fef8e0161
Murathan <mrthnalgr34@gmail.com>
2024-11-24 22:57:27 +0300

    Update page.tsx
    Started to design Login page

--- src/app/(dashboard)/sign-in/page.tsx
+++ src/app/(dashboard)/sign-in/page.tsx
@@ -1,6 +1,20 @@
+import { Form } from "react-hook-form"
+
 const LoginPage = () => {
   return (
-    <div className=''>LoginPage</div>
+    <div className='flex items-center w-[1920px] h-[1080px] bg-[#1f2021] text-white'>
+      <div className="bg-[#0e0e0e] mt-18 ml-48 box-border h-[402px] w-[1145px] border-4 black">
+
+        
+
+        <form className="flex grid grid-cols w-24 ml-[735px] mt-[150px] bg-black box-border">
+          <input type="email" placeholder="E-mail" className="bg-[#0e0e0e]"></input>
+          <input type="passw" placeholder="Password" className="hidden"></input>
+          <button className="bg-[#2447f2] text-white w-36 rounded-full ">Submit</button>
+        </form>
+      </div>
+      <div className="mt-[150px] text-white">Welcome to SchoolMrthn</div>
+    </div>
   )
 }
 
